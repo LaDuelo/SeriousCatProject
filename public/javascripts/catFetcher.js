e6bc2e3ed5017db5d00ef093b0ac66d2b5fd7fb2
LaDuelo <wikisupp@gmail.com>
2018-03-27 10:45:35 +0100

    Ignores and stuff

--- public/javascripts/catFetcher.js
+++ public/javascripts/catFetcher.js
@@ -8,10 +8,10 @@ module.exports = function () {
 
 const snoowrap = require('snoowrap');
 const r = new snoowrap({
-    userAgent: 'CatCrawler',
-    clientId: 'OFkITbcorJi-kA',
-    clientSecret: '1k23LOp-47dtB1th_SLwmiHxK0Y',
-    refreshToken: '27716321-ZsNxHJ-R5hNc3idwMJQkF1S54Qs'
+    userAgent: userAgent,
+    clientId: clientId,
+    clientSecret: clientSecret,
+    refreshToken: refreshToken
 });
 
 function filterByScore(posts) {
